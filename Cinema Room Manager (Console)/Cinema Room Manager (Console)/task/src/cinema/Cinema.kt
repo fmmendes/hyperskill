7835827a5bc3a2d30551cc0bf517b775769687b8
Filipe Moraes Mendes <10069692+fmmendes@users.noreply.github.com>
2023-03-06 00:28:33 -0300

    Stage 2

--- Cinema Room Manager (Console)/Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Console)/Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
@@ -1,18 +1,19 @@
 package cinema
 
-const val ROWS: Int = 7
-const val COLUMNS: Int = 8
-
 fun main() {
-    println("Cinema:")
-    for (row in 0..ROWS) {
-        for (column in 0..COLUMNS) {
-            if (row == 0 && column == 0) print(' ')
-            if (row == 0 && column != 0) print(column)
-            if (row != 0 && column == 0) print(row)
-            if (row != 0 && column != 0) print('S')
-            print(' ')
-            if (column == COLUMNS) print('\n')
-        }
+    println("Enter the number of rows:")
+    val rows: Int = readln().toInt()
+
+    println("Enter the number of seats in each row:")
+    val seatsPerRow: Int = readln().toInt()
+
+    val income: Int = if (rows * seatsPerRow <= 60) {
+        rows * seatsPerRow * 10
+    } else {
+        val frontSeats = (rows / 2) * seatsPerRow
+        val backSeats = ((rows / 2) + (rows % 2)) * seatsPerRow
+        (frontSeats * 10) + (backSeats * 8)
     }
+    println("Total income:\n$$income")
 }
+
