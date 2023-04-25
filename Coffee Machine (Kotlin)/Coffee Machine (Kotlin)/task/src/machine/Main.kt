0f698352dc82b6ffd435fa0f0c956ac07c3c9b08
Filipe Moraes Mendes <10069692+fmmendes@users.noreply.github.com>
2023-04-25 00:29:13 -0300

    Stage 2/6: Ingredient calculator
    Stage Completed

--- Coffee Machine (Kotlin)/Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -1,13 +1,19 @@
 package machine
 
+const val WATER_PER_CUP = 200
+const val MILK_PER_CUP = 50
+const val COFFEE_PER_CUP = 15
+
 fun main() {
-    println("""
-        Starting to make a coffee
-        Grinding coffee beans
-        Boiling water
-        Mixing boiled water with crushed coffee beans
-        Pouring coffee into the cup
-        Pouring some milk into the cup
-        Coffee is ready!
-    """.trimIndent())
+    println("Write how many cups of coffee you will need:")
+
+    val cups = readln().toInt()
+    val water = cups * WATER_PER_CUP
+    val milk = cups * MILK_PER_CUP
+    val coffee = cups * COFFEE_PER_CUP
+
+    println("For $cups cups of coffee you will need:")
+    println("$water ml of water")
+    println("$milk ml of milk")
+    println("$coffee g of coffee beans")
 }
