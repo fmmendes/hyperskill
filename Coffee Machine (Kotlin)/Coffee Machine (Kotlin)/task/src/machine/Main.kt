1ea1129b04ca6a727790dffc68af0b50cb8985fd
Filipe Moraes Mendes <10069692+fmmendes@users.noreply.github.com>
2023-05-09 22:41:01 -0300

    Stage 3 completed

--- Coffee Machine (Kotlin)/Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -4,16 +4,35 @@ const val WATER_PER_CUP = 200
 const val MILK_PER_CUP = 50
 const val COFFEE_PER_CUP = 15
 
+var waterSupply = 0 // ml
+var milkSupply = 0 // ml
+var coffeeSupply = 0 //g
+
 fun main() {
+    startMachine()
     println("Write how many cups of coffee you will need:")
+    val neededCups = readln().toInt()
 
-    val cups = readln().toInt()
-    val water = cups * WATER_PER_CUP
-    val milk = cups * MILK_PER_CUP
-    val coffee = cups * COFFEE_PER_CUP
+    val water = waterSupply / WATER_PER_CUP
+    val milk = milkSupply / MILK_PER_CUP
+    val coffee = coffeeSupply / COFFEE_PER_CUP
 
-    println("For $cups cups of coffee you will need:")
-    println("$water ml of water")
-    println("$milk ml of milk")
-    println("$coffee g of coffee beans")
+    val min = minOf(water, milk, coffee)
+
+    if (neededCups == min) {
+        println("Yes, I can make that amount of coffee")
+    } else if (neededCups < min) {
+        println("Yes, I can make that amount of coffee (and even ${min - neededCups} more than that)")
+    } else {
+        println("No, I can make only $min cups of coffee")
+    }
+}
+
+fun startMachine() {
+    println("Write how many ml of water the coffee machine has:")
+    waterSupply = readln().toInt()
+    println("Write how many ml of milk the coffee machine has:")
+    milkSupply = readln().toInt()
+    println("Write how many grams of coffee beans the coffee machine has:")
+    coffeeSupply = readln().toInt()
 }
